a13b96e627d54de8359e1488c884fd30ed916b21
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2025-01-14 10:31:40 -0500

    feat: :triangular_flag_on_post: se visualiza url blob en terminal

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     const videoElement = document.getElementById('video');
     const videoConfig = {
         url: 'media/vonepreview.mp4', 
@@ -24,43 +24,5 @@
         console.error('Error al cargar el video.');
     };
     xhr.send();
-    videoElement.addEventListener('contextmenu', (e) => e.preventDefault());
-});
- */
-
-document.addEventListener('DOMContentLoaded', function () {
-    const videoElement = document.getElementById('video');
-    const videoConfig = {
-        url: 'media/vonepreview.mp4',
-        type: 'video/mp4'
-    };
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', videoConfig.url, true);
-    xhr.responseType = 'blob';
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            const blob = xhr.response;
-            const url = URL.createObjectURL(blob);
-            const sourceElement = document.createElement('source');
-            sourceElement.src = url;
-            sourceElement.type = videoConfig.type;
-            videoElement.appendChild(sourceElement);
-            videoElement.load();
-
-            // Revoca el objeto URL después de cargarlo
-            videoElement.onloadeddata = () => {
-                URL.revokeObjectURL(url);
-                console.log('URL de blob revocada por seguridad.');
-            };
-        } else {
-            console.error('No se pudo cargar el video:', xhr.statusText);
-        }
-    };
-    xhr.onerror = function () {
-        console.error('Error al cargar el video.');
-    };
-    xhr.send();
-
-    // Seguridad adicional: Deshabilitar el menú contextual
     videoElement.addEventListener('contextmenu', (e) => e.preventDefault());
 });
